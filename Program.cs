e469a1cfa1ed7aee9120982bf53b3c1d206c1731
gerel <gerel@bk.ru>
2023-12-12 19:04:59 +0500

    Add CreateRandomArray

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿void FillArray(int[] arr)
+﻿void FillArray(int[] arr, int start, int end)
 {
     for (int i=0; i<arr.Length; i++)
     {
-        arr[i] = new Random().Next(1, 100);
+        arr[i] = new Random().Next(start, end);
     }
 }
+int[] CreateRandomArray(int size, int start, int end)
+{
+    int[] result = new int[size];
+    FillArray(result, start, end);
+    return result;
+}
+int[] arr = CreateRandomArray(10, 1, 100);
+Console.WriteLine($"[{string.Join(", ", arr)}]");
